9cfdcb46f077f1423792129676f2778302474ea2
keita03301995 <keita03301995@gmail.com>
2016-08-01 21:38:04 +0900

    zako

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
- <head>
-  <title>doutei botti</title>
- <head>
- <body>
- <?
-php echo '<p>Hello zakodomo</p>'
-; ?>
- </body>
+  <head>
+    <title>doutei botti</title>
+  </head>
+  <body>
+      <?php
+        echo '<p>Hello zakodomo</p>';
+      ?>
+  </body>
 </html>
